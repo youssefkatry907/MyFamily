3ea60b570f0ca30a295c1cb04fb308dd557519ce
A7mdlbanna <ahmedbanna200@gmail.com>
2023-03-18 08:14:39 +0200

    update firebase functions index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
 const functions = require("firebase-functions");
+const myApp = require("../config/server"); // Causes Error (can't find file)
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+
+exports.family = functions.https.onRequest(myApp);
