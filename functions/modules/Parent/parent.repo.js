606b6b6044d9ccfffded410b93f82fe7ac179249
youssefkatry907 <youssefkatry907@gmail.com>
2023-04-26 02:17:46 +0200

    FamilyUserName

--- functions/modules/Parent/parent.repo.js
+++ functions/modules/Parent/parent.repo.js
@@ -43,7 +43,7 @@ exports.add = async (form, parentId) => {
                 for (let i = 0; i < form.membersNum; i++) {
                     var child = new Child({
                         parent: parentId,
-                        familyUserName: parent.record.familyUsername,
+                        familyUserName: parent.record.familyUserName,
                         email: form.members[i].email,
                         familyPassword: parent.record.familyPassword
                     });
@@ -54,7 +54,7 @@ exports.add = async (form, parentId) => {
                 for (let i = 0; i < form.membersNum; i++) {
                     var helper = new Helper({
                         parent: parentId,
-                        familyUserName: parent.record.familyUsername,
+                        familyUserName: parent.record.familyUserName,
                         email: form.members[i].email,
                         permissions: form.members[i].permissions,
                         familyPassword: parent.record.familyPassword
