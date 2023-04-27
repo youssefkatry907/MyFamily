bb5bfc596a719d983045208e0124468e605b97fb
youssefkatry907 <youssefkatry907@gmail.com>
2023-04-27 02:02:32 +0200

    Vote and add Enter Done

--- functions/routes/entertainment/entertainment.route.js
+++ functions/routes/entertainment/entertainment.route.js
@@ -3,6 +3,7 @@ const app = require('express').Router()
 const entertainmentController = require('../../controllers/entertainment/entertainment.controller')
 
 app.post("/addEntertainment", entertainmentController.addEntertainment);
+app.post("/vote", entertainmentController.voteSuggestion);
 app.get("/getAllEntertainment", entertainmentController.getAllEntertainment);
 app.delete("/removeEntertainment", entertainmentController.removeEntertainment);
 
