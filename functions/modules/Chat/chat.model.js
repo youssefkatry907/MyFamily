33104da9103678d548ed6e72e5cd37b21d7659dc
youssefkatry907 <youssefkatry907@gmail.com>
2023-05-05 00:44:56 +0300

    Endpoints for chat Done

--- functions/modules/Chat/chat.model.js
+++ functions/modules/Chat/chat.model.js
@@ -1,10 +1,12 @@
 let mongoose = require('mongoose');
 
 let chatSchema = mongoose.Schema({
-    sender: { type: mongoose.Types.ObjectId, ref: 'childrens', required: true },
-    receiver: { type: mongoose.Types.ObjectId, ref: 'childrens', required: true },
+    sender: { type: mongoose.Types.ObjectId, required: true },
+    senderName: { type: String, required: true },
+    receiver: { type: mongoose.Types.ObjectId, required: true },
     message: { type: String, required: true },
     date: { type: Date, default: Date.now },
+    image: { type: String },
     // seen: { type: Boolean, default: false },
 });
 
--- functions/routes/index.route.js
+++ functions/routes/index.route.js
@@ -4,13 +4,13 @@ let parentRoutes = require("./parent/parent.route");
 let entertainmentRoutes = require("./entertainment/entertainment.route");
 let todoRoutes = require("./todo/todo.route");
 let childRoutes = require("./child/index.route");
+let helperRoutes = require("./helper/index.route");
 let studyRoutes = require("./study/study.route");
-let helperRoutes = require("./helper/helper.route");
-let chatRoutes = require("./chat/chat.route");
 let groupRoutes = require("./group/group.route");
 let eventRoutes = require("./event/event.route");
+
 let parentNotificationRoutes = require("./parent/notification.route");
-let helperNotificationRoutes = require("./helper/notification.route");
+let parentChatRoutes = require("./parent/chat.route");
 
 app.get("/", (req, res) => {
     res.status(200).json({ success: true, message: "Welcome to myFamily Server!", code: 200 })
@@ -20,13 +20,14 @@ app.use("/api/v1/parent", parentRoutes);
 app.use("/api/v1/entertainment", entertainmentRoutes);
 app.use("/api/v1/todo", todoRoutes);
 app.use("/api/v1/child", childRoutes);
-app.use("/api/v1/study", studyRoutes);
 app.use("/api/v1/helper", helperRoutes);
-app.use("/api/v1/chat", chatRoutes);
+app.use("/api/v1/study", studyRoutes);
 app.use("/api/v1/group", groupRoutes);
 app.use("/api/v1/event", eventRoutes);
+
 app.use("/api/v1/notification", parentNotificationRoutes);
-app.use("/api/v1/notification", helperNotificationRoutes)
+app.use("/api/v1/chat", parentChatRoutes);
+
 
 
 
