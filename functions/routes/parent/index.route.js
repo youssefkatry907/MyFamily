298fd692a52399a63dd34e069d050ad74756d2f3
youssefkatry907 <youssefkatry907@gmail.com>
2023-05-05 02:02:51 +0300

    Change group directories

--- functions/routes/parent/index.route.js
+++ functions/routes/parent/index.route.js
@@ -8,10 +8,12 @@ const allowedRoles = ['parent'];
 const parentRoutes = require('./parent.route');
 const notificationRoutes = require('./notification.route');
 const chatRoutes = require('./chat.route');
+const groupRoutes = require('./group.route');
 
 app.use('/parent', checkToken(allowedRoles), parentRoutes);
 app.use('/notification', checkToken(allowedRoles), notificationRoutes);
 app.use('/chat', checkToken(allowedRoles), chatRoutes);
+app.use('/group', checkToken(allowedRoles), groupRoutes);
 
 
 module.exports = app;
