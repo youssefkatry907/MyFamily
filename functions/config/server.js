8f6582d078fe2c99ad4a9249a3dbf533abe2cf39
youssefkatry907 <youssefkatry907@gmail.com>
2023-05-07 18:25:16 +0300

    Group model and  socket

--- functions/config/server.js
+++ functions/config/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 const chatRepo = require("../modules/Chat/chat.repo")
-const chatController = require("../controllers/parent/chat.controller")
+const groupRepo = require("../modules/Group/group.repo")
 
 let server = app.listen(8000, () => {
     console.log(`Server is up and runing on port 8000!`)
@@ -22,11 +22,6 @@ io.on('connection', (socket) => {
         console.log(msg);
     });
 
-    socket.on('deleteMessage', async (msg) => {
-        const result = await chatRepo.remove(msg.messageId);
-        io.emit('deleteMessage', result);
-    })
-
     socket.on('listMessages', async (chatId) => { // list
         const chat = await chatRepo.find({ _id: chatId });
         console.log(chat);
@@ -34,6 +29,31 @@ io.on('connection', (socket) => {
         io.emit('listMessages', messages); //listen , msg ==> receiver, text
         console.log(messages);
     });
+
+    socket.on('sendGroupMessage', async (msg) => {
+        let check = await groupRepo.sendMessage(msg);
+        let groupId;
+        if (check.success) {
+            groupId = msg.groupId;
+            const group = await groupRepo.get({ _id: groupId });
+            const messages = group.record.messages;
+            io.emit('listGroupMessages', messages);
+        }
+        console.log(msg);
+    });
+
+    socket.on('listGroupMessages', async (groupId) => { // list
+        const group = await groupRepo.get({ _id: groupId });
+        console.log(group);
+        const messages = group.record.messages;
+        io.emit('listGroupMessages', messages); 
+        console.log(messages);
+    });
+
+    socket.on('deleteMessage', async (msg) => {
+        const result = await chatRepo.remove(msg.messageId);
+        io.emit('deleteMessage', result);
+    })
 
     socket.on('disconnected', () => {
         console.log("socket.io : User disconnected: ", socket.id);
--- functions/modules/Group/group.repo.js
+++ functions/modules/Group/group.repo.js
@@ -1,6 +1,28 @@
 let Group = require('./group.model');
 const jwt = require('jsonwebtoken');
 
+exports.isExist = async (_id) => {
+    if (!_id) {
+        return {
+            success: false,
+        }
+    }
+    const record = await Group.findOne({ _id });
+    if (record) {
+        return {
+            success: true,
+            code: 200,
+            Group: record
+        };
+    } else {
+        return {
+            code: 404,
+            success: false,
+            error: "Group not found!",
+        }
+    }
+}
+
 exports.list = async (familyUserName) => {
     try {
         let groups = await Group.find({ familyUserName });
@@ -37,13 +59,13 @@ exports.create = async (form) => {
     }
 }
 
-exports.get = async (id) => {
+exports.get = async (_id) => {
     try {
-        let group = Group.findById(id);
+        let record = Group.findOne({ _id });
         return {
             success: true,
             code: 200,
-            group
+            record
         };
     }
     catch (error) {
@@ -52,5 +74,62 @@ exports.get = async (id) => {
             code: 500,
             error: "Unexpected Error!"
         };
+    }
+}
+
+exports.sendMessage = async (msg) => {
+    try {
+
+        let groupId = await this.isExist({ _id: msg.groupId });
+        //console.log(groupId);
+        if (groupId.success) {
+            const message = {
+                sender: msg.sender,
+                senderName: msg.senderName,
+                message: msg.message,
+                date: msg.messageDate
+            }
+            let newMessage = await Group.findOneAndUpdate({ _id: groupId.Group._id }, { $push: { messages: message } }, { new: true });
+            await newMessage.save();
+            //console.log(newMessage);
+            return {
+                success: true,
+                groupId: groupId.Group._id
+            }
+        }
+        else {
+            return {
+                success: false
+            }
+        }
+        // else {
+        //     const chat = {
+        //         sender: msg.sender,
+        //         senderName: msg.senderName,
+        //         receiver: msg.receiver,
+        //         receiverName: msg.receiverName,
+        //         lastMessage: msg.message,
+        //         messages: [
+        //             {
+        //                 sender: msg.sender,
+        //                 receiver: msg.receiver,
+        //                 message: msg.message,
+        //                 date: msg.messageDate
+        //             }
+        //         ],
+        //         lastMessageDate: msg.messageDate,
+        //         senderImage: msg.senderImage,
+        //         receiverImage: msg.receiverImage
+        //     }
+        //     console.log(chat);
+        //     let newChat = await this.create(chat);
+        //     console.log(newChat);
+        //     return {
+        //         create: true,
+        //         groupId: newChat.record._id
+        //     }
+        // }
+    } catch (err) {
+        console.log(err.message, err.message);
     }
 }
