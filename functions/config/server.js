926d9d5dd4305484daf60106eaf7dfca12fec529
youssefkatry907 <youssefkatry907@gmail.com>
2023-05-16 01:39:56 +0300

    Socket.js

--- functions/config/server.js
+++ functions/config/server.js
@@ -1,58 +1,26 @@
 const app = require('./app')
-const chatRepo = require("../modules/Chat/chat.repo")
-const groupRepo = require("../modules/Group/group.repo")
+const socketIo = require('socket.io')
+const http = require('http')
+const socketHandler = require('./socket')
 
-let server = app.listen(8000, () => {
-    console.log(`Server is up and runing on port 8000!`)
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Authorization', 'Content-Type']
+};
+
+//connect server
+
+let server = http.createServer(app)
+
+let io = socketIo(server, {
+    cors: corsOptions
 })
 
-var io = require('socket.io')(server);
+socketHandler(io)
 
-io.on('connection', (socket) => {
+server.listen(4000, () => {
+    console.log('listening on 8000!')
+})
 
-
-    console.log('a user connected');
-
-    socket.on('sendMessage', async (msg) => {
-        let check = await chatRepo.sendMessage(msg);
-        let chatId = check.create ? check.chatId : msg.chatId
-        const chat = await chatRepo.find({ _id: chatId });
-        const messages = chat.records.messages;
-        io.emit('listMessages', messages);
-    });
-
-    socket.on('listMessages', async (id) => { // list
-        const chat = await chatRepo.find(id.chat ? { _id: id.chat } : {sender: id.sender});
-        const messages = chat.records ? chat.records.messages : [];
-        io.emit('listMessages', messages); //listen , msg ==> receiver, text
-    });
-
-    socket.on('sendGroupMessage', async (msg) => {
-        let check = await groupRepo.sendMessage(msg);
-        let groupId;
-        if (check.success) {
-            groupId = msg.chatId;
-            const group = await groupRepo.get({ _id: groupId });
-            const messages = group.record.messages;
-            io.emit('listGroupMessages', messages);
-        }
-    });
-
-    socket.on('listGroupMessages', async (groupId) => { // list
-        const group = await groupRepo.get({ _id: groupId });
-        const messages = group.record.messages;
-        io.emit('listGroupMessages', messages);
-    });
-
-    socket.on('deleteMessage', async (msg) => {
-        const result = await chatRepo.remove(msg.messageId);
-        io.emit('deleteMessage', result);
-    })
-
-    socket.on('disconnected', () => {
-        console.log("socket.io : User disconnected: ", socket.id);
-    })
-
-});
-
-module.exports = app;
+module.exports = server;
