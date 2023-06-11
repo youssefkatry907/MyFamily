731e1aaa3b15588ddddccb21ba91e65cab9b5311
youssefkatry907 <youssefkatry907@gmail.com>
2023-06-11 16:47:41 +0300

    Deploy on heroku

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require("firebase-functions");
-const app = require("./config/app");
-
-// const admin = require("firebase-admin");
-// admin.initializeApp();
-
-
-exports.familyFinal = functions.https.onRequest(app)
